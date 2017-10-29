87cc459605b34dcf1adc1e890228124073a7a481
Tyson Lin <tsong99@gmail.com>
2017-10-29 19:28:06 +0700

    cosmetic and error msg changes

--- server/auth/user.js
+++ server/auth/user.js
@@ -7,6 +7,8 @@ const User = require('../models').User;
 const { HttpUnauthorized, HttpNotFound } = require('../validators/errors');
 const { shouldExist } = require('../validators');
 
+const env = process.env.NODE_ENV || 'development';
+
 const shouldExistAuthUser = (err_msg, data) => {
     return shouldExist(err_msg, data)
         .catch( err => {
@@ -22,14 +24,14 @@ module.exports = {
         var authUser;
 
         return User.findOne( { where: {username: username} } )
-            .then(shouldExistAuthUser.bind(null, `Invalid Login - [${username}] not found !!!DELETE THIS AFTER DEBUG!!!`))
+            .then(shouldExistAuthUser.bind(null, 'Invalid login credentials' + ((env === 'development')?` - [${username}] not found`:'') ))
             .then( user => { 
                 authUser = user;
                 return User.comparePassword(candidatePwd, user.password); 
             })
             .then( isMatch => {
                 if(!isMatch)
-                    throw new HttpUnauthorized(`Invalid Login - incorrect password !!!DELETE THIS AFTER DEBUG!!!`);
+                    throw new HttpUnauthorized('Invalid login credentials' + ((env === 'development')?' - incorrect password':'') );
                 else {
                     const token = jwt.sign({data: authUser}, config.secret, {
                         expiresIn: 604800 //1 week
